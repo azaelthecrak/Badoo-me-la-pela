4bacb0594c35e0585bc5d4dee036c513d30483a7
Rafael <rafael_zamora99@hotmail.com>
2017-11-05 14:17:31 -0600

    Layout
    layout v1

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     <title>Nose</title>
   </head>
   <body>
+<nav>
 
+</nav>
+<div class="inicio">
+  <p>Frase chida</p>
+    <div class="F-login">
+      <form class="Login" action="index.html" method="post">
+        <h1>Registrate</h1>
+          <input type="text" name="" value="" placeholder="correo">
+          <input type="text" name="" value="" placeholder="contraseña">
+          <input type="text" name="" value="" placeholder="nombre de usuario">
+          <input type="submit" name="" value="">
+      </form>
+    </div>
+</div>
+<div class="inicio-footer">
+  <p>Aqui va lo que ofrecemos XD</p>
+</div>
   </body>
 </html>
