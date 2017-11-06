fb345c55dfe7d8214a0d2adfd0b8ce0023276f89
Rafael <rafael_zamora99@hotmail.com>
2017-11-05 18:21:16 -0600

    Navegacion
    Menu de navegacion

--- index.php
+++ index.php
@@ -3,11 +3,32 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Nose</title>
   </head>
   <body>
-<nav>
-
+<nav class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toogle" data-toogle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+  </div>
+  <div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav collapse">
+      <li><a href="index.php">Logo</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-center">
+      <li><a href="#">Cosa 1</a></li>
+      <li><a href="#">Cosa 2</a></li>
+      <li><a href="#">Cosa 3</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Usuario</a></li>
+      <li><a href="#">Cerrar sesion</a></li>
+    </ul>
+  </div>
 </nav>
 <div class="inicio">
   <p>Frase chida</p>
